6a6403c46606850dd5f2f2aa29039c0939111584
Manish Garg <manish.garg@udaan.com>
2021-05-01 18:57:38 +0530

    Fix Eslint files

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+// eslint-disable-next-line react/prop-types
+const MyApp = ({Component, pageProps}) => {
+  return <Component {...pageProps} />;
+};
 
-export default MyApp
+export default MyApp;
